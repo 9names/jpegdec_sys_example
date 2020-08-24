c09dca7de169e4d7501f49791736be6c21993357
9names <60134748+9names@users.noreply.github.com>
2020-08-24 19:25:27 +1000

    Ran code through Cargo::fmt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use jpegdec_sys::*;
-use std::time::{SystemTime};
+use std::time::SystemTime;
 
 // Bundle tulips.jpg in our binary
 const TULIPS_CONST: &[u8; 56010] = include_bytes!("tulips.jpg");
@@ -7,24 +7,27 @@ const TULIPS_CONST_PTR: *const u8 = TULIPS_CONST.as_ptr();
 //let tulipsptr = TULIPS_CONST.as_ptr();
 
 ///TODO: write a real draw callback
-extern "C" fn callback(_pdraw: *mut JPEGDRAW) {
-    
-}
+extern "C" fn callback(_pdraw: *mut JPEGDRAW) {}
 // Must wrap our callback function
 const DRAW_CALLBACK: JPEG_DRAW_CALLBACK = Some(callback);
 
 fn main() {
     // Full size
-    {         
+    {
         let start = SystemTime::now();
         let image = Box::new(unsafe { JPEG_ZeroInitJPEGIMAGE() });
         let imgptr: *mut JPEGIMAGE = Box::into_raw(image);
-    
+
         // include_bytes gives us an immutable slice, copy that into a mutable one if using unmodified JPEG_openRAM
         // let tulips: &mut [u8] = &mut TULIPS_CONST.clone();
         // let tulipsptr = tulips.as_mut_ptr();
         unsafe {
-            let opened = JPEG_openRAM(imgptr, TULIPS_CONST_PTR, TULIPS_CONST.len() as i32, DRAW_CALLBACK);
+            let opened = JPEG_openRAM(
+                imgptr,
+                TULIPS_CONST_PTR,
+                TULIPS_CONST.len() as i32,
+                DRAW_CALLBACK,
+            );
             if opened != 0 {
                 let rc = JPEG_decode(imgptr, 0, 0, 0);
                 if rc != 0 {
@@ -38,13 +41,18 @@ fn main() {
             }
         }
     }
-    // Half size 
+    // Half size
     {
         let start = SystemTime::now();
         let image = Box::new(unsafe { JPEG_ZeroInitJPEGIMAGE() });
         let imgptr: *mut JPEGIMAGE = Box::into_raw(image);
         unsafe {
-            let opened = JPEG_openRAM(imgptr, TULIPS_CONST_PTR, TULIPS_CONST.len() as i32, DRAW_CALLBACK);
+            let opened = JPEG_openRAM(
+                imgptr,
+                TULIPS_CONST_PTR,
+                TULIPS_CONST.len() as i32,
+                DRAW_CALLBACK,
+            );
             if opened != 0 {
                 let rc = JPEG_decode(imgptr, 0, 0, JPEG_SCALE_HALF as i32);
                 if rc != 0 {
@@ -56,15 +64,20 @@ fn main() {
                 let errstr = JPEG_getLastError(imgptr);
                 println!("Last error: {}", errstr);
             }
-        }     
+        }
     }
-    // Quarter size 
+    // Quarter size
     {
         let start = SystemTime::now();
         let image = Box::new(unsafe { JPEG_ZeroInitJPEGIMAGE() });
         let imgptr: *mut JPEGIMAGE = Box::into_raw(image);
         unsafe {
-            let opened = JPEG_openRAM(imgptr, TULIPS_CONST_PTR, TULIPS_CONST.len() as i32, DRAW_CALLBACK);
+            let opened = JPEG_openRAM(
+                imgptr,
+                TULIPS_CONST_PTR,
+                TULIPS_CONST.len() as i32,
+                DRAW_CALLBACK,
+            );
             if opened != 0 {
                 let rc = JPEG_decode(imgptr, 0, 0, JPEG_SCALE_QUARTER as i32);
                 if rc != 0 {
@@ -76,6 +89,6 @@ fn main() {
                 let errstr = JPEG_getLastError(imgptr);
                 println!("Last error: {}", errstr);
             }
-        }     
+        }
     }
 }
