661a68779497d43a4838aebfd15231212ad709bf
9names <60134748+9names@users.noreply.github.com>
2020-09-08 00:15:46 +1000

    Converted example to use embedded_graphics_simulator

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,25 @@
 use jpegdec_sys::JPEGDRAW;
 use jpegdec_sys::*;
-use minifb::{Key, Window, WindowOptions};
+
+use embedded_graphics::{
+    image::{Image, ImageRaw},
+    pixelcolor::{raw::LittleEndian, Rgb565},
+    prelude::*,
+};
+
+use embedded_graphics_simulator::{
+    OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
+};
+
+use sdl2::keyboard::Keycode;
 use std::time::SystemTime;
 
-// Bundle tulips.jpg in our binary
-const TULIPS_CONST: &[u8; 56010] = include_bytes!("tulips.jpg");
-const TULIPS_CONST_PTR: *const u8 = TULIPS_CONST.as_ptr();
+const TULIPS_CONST: &[u8; 5184] = include_bytes!("rust-pride.jpg");
 
-const WIDTH: usize = 640;
-const HEIGHT: usize = 480;
-
-/// Need our framebuffer to be accessible in the C callback :(
-static mut FB: Vec<u32> = Vec::new();
-
-/// Convert RGB565 (bit-packed in u16) to RGB888 (bit-packed in u32)
-/// Our source JPG is RGB565, but minifb wants 24bit RGB data stored in a 32bit field
-/// Note that this is using a simple shift to bring the elements into range,
-/// but it will never reach full saturation (therefore color will never be quite correct)
-/// Most RGB565 displays aren't that accurate anyway, but it's worth noting
-fn rgb565_to_rgb888(pixel: u16) -> u32 {
-    // Unpack our 5bit and 6bit RGB elements
-    let r = 0b0000_0000_0001_1111 & pixel >> 11;
-    let g = 0b0000_0000_0011_1111 & pixel >> 5;
-    let b = 0b0000_0000_0001_1111 & pixel;
-    // Shift them up so they can get close to full scale
-    let r8 = (r << 3) as u32;
-    let g8 = (g << 2) as u32;
-    let b8 = (b << 3) as u32;
-    // And bit-pack them into a u32 again
-    r8 << 16 | g8 << 8 | b8
-}
+// Need our target buffer width for the callback
+const IMGBUF_WIDTH: usize = 64;
+const IMGBUF_HEIGHT: usize = 64;
+static mut IMGBUF: [u16; 4096] = [0; 4096];
 
 /// Callback function for passing to JPEGDEC that will handle drawing
 extern "C" fn callback(p_draw: *mut JPEGDRAW) {
@@ -44,55 +34,42 @@ extern "C" fn callback(p_draw: *mut JPEGDRAW) {
 
     for y in 0..draw_height {
         // Since we're using byte indexes into single dimension objects for display, we need to calcuate
-        // how far through we are for each x/y position. This is going to depend on the width of the 
-        // buffer for both the source (JPEG) and destination (framebuffer) 
+        // how far through we are for each x/y position. This is going to depend on the width of the
+        // buffer for both the source (JPEG) and destination (framebuffer)
         let src_y_offset = y * draw_width;
-        let dst_y_offset = (y + start_y) * WIDTH;
+        let dst_y_offset = (y + start_y) * IMGBUF_WIDTH;
         for x in 0..draw_width {
             let src_offset = x + src_y_offset;
             let dst_offset = x + dst_y_offset + start_x;
             unsafe {
-                let pix = rgb565_to_rgb888(*pixel_data.add(src_offset));
-                FB[dst_offset] = pix;
+                IMGBUF[dst_offset] = *pixel_data.add(src_offset);
             }
         }
     }
 }
 
 fn main() {
-    unsafe {
-        // Can't const init a static Vec yet, so replace the FB here
-        FB = vec![0; WIDTH * HEIGHT]
-    }
+    assert_eq!(unsafe { IMGBUF.len() }, IMGBUF_WIDTH * IMGBUF_HEIGHT);
+    let window_size = Size::new(64, 64);
 
-    // Create our window so we've got somewhere to put our pixels
-    let mut window = Window::new(
-        "JPEGDEC demo - ESC to exit",
-        WIDTH,
-        HEIGHT,
-        WindowOptions::default(),
-    )
-    .unwrap_or_else(|e| {
-        panic!("{}", e);
-    });
-    window.limit_update_rate(Some(std::time::Duration::from_micros(1_000_000 / 60)));
+    let mut display: SimulatorDisplay<Rgb565> = SimulatorDisplay::new(window_size);
+    let output_settings = OutputSettingsBuilder::new().build();
+    let mut window = Window::new("JPEGDEC test", &output_settings);
 
     // Set up our JPEGDEC objects
-    let image = Box::new(unsafe { JPEG_ZeroInitJPEGIMAGE() });
-    let imgptr: *mut JPEGIMAGE = Box::into_raw(image);
-    const DRAW_CALLBACK: JPEG_DRAW_CALLBACK = Some(callback);
+    let mut image = unsafe { JPEG_ZeroInitJPEGIMAGE() };
+    let imgptr: *mut JPEGIMAGE = &mut image as *mut JPEGIMAGE;
 
-    // Main event loop
-    while window.is_open() && !window.is_key_down(Key::Escape) {
+    'running: loop {
+        window.update(&display);
+
         let start = SystemTime::now();
-        // Now onto the main "unsafe" stuff - decoding the image and drawing it!
-        // Need an unsafe around JPEG_openRAM, JPEG_decode, and JPEG_getLastError becuase it's all C
         unsafe {
             let opened = JPEG_openRAM(
                 imgptr,
-                TULIPS_CONST_PTR,
+                TULIPS_CONST.as_ptr(),
                 TULIPS_CONST.len() as i32,
-                DRAW_CALLBACK,
+                Some(callback),
             );
             if opened != 0 {
                 let rc = JPEG_decode(imgptr, 0, 0, 0);
@@ -106,11 +83,21 @@ fn main() {
                 println!("Last error: {}", errstr);
             }
         }
-        // Seperate unsafe block for our buffer update - it's only unsafe because it uses FB.
-        // If we could use a local buffer, Rust would be able to do it's lifetime/thread safety checks
-        // and we wouldn't need this.
-        unsafe {
-            window.update_with_buffer(&FB, WIDTH, HEIGHT).unwrap();
+        let imgbuf_u8slice = unsafe { core::mem::transmute::<[u16; 4096], [u8; 8192]>(IMGBUF) };
+        let image_raw: ImageRaw<Rgb565, LittleEndian> = ImageRaw::new(&imgbuf_u8slice, 64, 64);
+        let img: Image<_, Rgb565> = Image::new(&image_raw, Point::zero());
+        img.draw(&mut display).unwrap();
+
+        for event in window.events() {
+            match event {
+                SimulatorEvent::KeyDown { keycode, .. } => {
+                    if keycode == Keycode::Escape {
+                        break 'running;
+                    }
+                }
+                SimulatorEvent::Quit => break 'running,
+                _ => {}
+            }
         }
     }
 }
